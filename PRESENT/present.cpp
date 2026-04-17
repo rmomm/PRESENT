3ef2671dcc70668818ce4487baab9169cc6be53a
Karina Malij <karinamalij0303@gmail.com>
2026-04-17 22:56:48 +0300

    S-box

---
+++ PRESENT/present.cpp
@@ -0,0 +1,47 @@
+#include "present.h"
+
+static const uint8_t S[16] = {
+    0xC, 0x5, 0x6, 0xB,
+    0x9, 0x0, 0xA, 0xD,
+    0x3, 0xE, 0xF, 0x8,
+    0x4, 0x7, 0x1, 0x2
+};
+
+static const uint8_t S_inv[16] = {
+    0x5, 0xE, 0xF, 0x8,
+    0xC, 0x1, 0x2, 0xD,
+    0xB, 0x4, 0x6, 0x3,
+    0x0, 0x7, 0x9, 0xA
+};
+
+uint8_t sBox(uint8_t v) {
+    return S[v & 0xF];
+}
+
+uint8_t invsBox(uint8_t v) {
+    return S_inv[v & 0xF];
+}
+
+uint64_t sBoxLayer(uint64_t state) {
+    uint64_t result = 0;
+
+    for (int i = 0; i < 16; i++) {
+        uint8_t n = (state >> (i * 4)) & 0xF;
+        uint8_t s = sBox(n);
+        result |= ((uint64_t)s << (i * 4));
+    }
+
+    return result;
+}
+
+uint64_t invsBoxLayer(uint64_t state) {
+    uint64_t result = 0;
+
+    for (int i = 0; i < 16; i++) {
+        uint8_t n = (state >> (i * 4)) & 0xF;
+        uint8_t s = invsBox(n);
+        result |= ((uint64_t)s << (i * 4));
+    }
+
+    return result;
+}
---
+++ PRESENT/present.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <cstdint>
+
+using namespace std;
+
+uint8_t sBox(uint8_t v);
+uint8_t invsBox(uint8_t v);
+
+uint64_t sBoxLayer(uint64_t state);
+uint64_t invsBoxLayer(uint64_t state);
