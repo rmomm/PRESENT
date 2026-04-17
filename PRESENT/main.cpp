f7df442a9e12a2332c68149e525364c9eef89b46
Karina Malij <karinamalij0303@gmail.com>
2026-04-17 22:58:07 +0300

    main1

---
+++ PRESENT/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "present.h"
+
+int main() {
+    uint64_t test = 0x0123456789ABCDEF;
+
+    uint64_t enc = sBoxLayer(test);
+    uint64_t dec = invsBoxLayer(enc);
+
+    cout << hex << test << endl;
+    cout << enc << endl;
+    cout << dec << endl;
+
+    return 0;
+}
