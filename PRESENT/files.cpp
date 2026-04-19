de525cf39f03351e23b29cc8ee6039f353153b5f
Karina Malij <karinamalij0303@gmail.com>
2026-04-19 22:31:58 +0300

    переробила readKey, щоб працювало для 80біт та 128біт

--- PRESENT/files.cpp
+++ PRESENT/files.cpp
@@ -3,8 +3,8 @@
 
 vector<uint8_t> readFile(const string& filename) {
     ifstream in(filename, ios::binary);
-    if (!in) {
-        throw runtime_error("Cannot open input file");
+    if (!in) { 
+        throw runtime_error("Cannot open input file"); 
     }
 
     in.seekg(0, ios::end);
@@ -20,73 +20,90 @@ vector<uint8_t> readFile(const string& filename) {
 void writeFile(const string& filename, const vector<uint8_t>& data) {
     ofstream out(filename, ios::binary);
     if (!out) { 
-        throw runtime_error("Cannot open output file");
+        throw runtime_error("Cannot open output file"); 
     }
 
     out.write(reinterpret_cast<const char*>(data.data()), data.size());
 }
 
-bitset<80> readKey80(const string& filename) {
+
+KeyData readKey(const string& filename) {
     ifstream in(filename);
-    if (!in) {
-        throw runtime_error("Cannot open key file");
+    if (!in) { 
+        throw runtime_error("Cannot open key file"); 
     }
 
     string hex;
     in >> hex;
 
-    if (hex.size() != 20) {
-        throw runtime_error("Key must be 80-bit");
-    }
+    KeyData kd;
 
-    bitset<80> key;
+    if (hex.size() == 20) {
+        kd.is128 = false;
 
-    for (int i = 0; i < 20; i++) {
-        uint8_t val = std::stoi(hex.substr(i, 1), nullptr, 16);
-        for (int j = 0; j < 4; j++) {
-            key[79 - (i * 4 + j)] = (val >> (3 - j)) & 1;
+        for (int i = 0; i < 20; i++) {
+            uint8_t val = stoi(hex.substr(i, 1), nullptr, 16);
+            for (int j = 0; j < 4; j++) {
+                kd.key80[79 - (i * 4 + j)] = (val >> (3 - j)) & 1;
+            }
         }
     }
+    else if (hex.size() == 32) {
+        kd.is128 = true;
 
-    return key;
+        for (int i = 0; i < 32; i++) {
+            uint8_t val = stoi(hex.substr(i, 1), nullptr, 16);
+            for (int j = 0; j < 4; j++) {
+                kd.key128[127 - (i * 4 + j)] = (val >> (3 - j)) & 1;
+            }
+        }
+    }
+    else {
+        throw runtime_error("Key must be 80 or 128-bit hex");
+    }
+
+    return kd;
+}
+
+vector<uint64_t> getRoundKeys(const KeyData& kd) {
+    if (kd.is128){ 
+        return generateRoundKeys128(kd.key128); 
+    } 
+    else {
+        return generateRoundKeys80(kd.key80);
+    }
 }
 
 
 void encryptFile(const string& inputFile, const string& outputFile, const string& keyFile) {
-
     auto data = readFile(inputFile);
-    auto key = readKey80(keyFile);
+    auto kd = readKey(keyFile);
 
-    auto roundKeysV = generateRoundKeys80(key);
+    auto keys = getRoundKeys(kd);
 
     uint64_t roundKeys[32];
-    for (int i = 0; i < 32; i++) {
-        roundKeys[i] = roundKeysV[i];
-    }
+    for (int i = 0; i < 32; i++) roundKeys[i] = keys[i];
 
     auto encrypted = encryptData(data, roundKeys);
 
     writeFile(outputFile, encrypted);
 
-    cout << "Encryption completed.\n";
+    cout << "Encryption done!\n";
 }
 
 
 void decryptFile(const string& inputFile, const string& outputFile, const string& keyFile) {
-
     auto data = readFile(inputFile);
-    auto key = readKey80(keyFile);
+    auto kd = readKey(keyFile);
 
-    auto roundKeysV = generateRoundKeys80(key);
+    auto keys = getRoundKeys(kd);
 
     uint64_t roundKeys[32];
-    for (int i = 0; i < 32; i++) {
-        roundKeys[i] = roundKeysV[i];
-    }
+    for (int i = 0; i < 32; i++) roundKeys[i] = keys[i];
 
     auto decrypted = decryptData(data, roundKeys);
 
     writeFile(outputFile, decrypted);
 
-    cout << "Decryption completed.\n";
+    cout << "Decryption done!\n";
 }
--- PRESENT/files.h
+++ PRESENT/files.h
@@ -5,13 +5,21 @@
 #include <fstream>
 #include <iostream>
 #include <cstdint>
+#include <stdexcept>
 
 using namespace std;
+
+struct KeyData {
+    bool is128 = false;;
+    bitset<80> key80;
+    bitset<128> key128;
+};
 
 vector<uint8_t> readFile(const string& filename);
 void writeFile(const string& filename, const vector<uint8_t>& data);
 
-bitset<80> readKey80(const string& filename);
+KeyData readKey(const string& filename);
+vector<uint64_t> getRoundKeys(const KeyData& kd);
 
 void encryptFile(const string& inputFile, const string& outputFile, const string& keyFile);
 void decryptFile(const string& inputFile, const string& outputFile, const string& keyFile);
