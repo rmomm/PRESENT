a7e8c4586848c9074f39aea601c930ea1fdea450
Karina Malij <karinamalij0303@gmail.com>
2026-04-19 08:51:27 +0300

    hhh

--- PRESENT/present.h
+++ PRESENT/present.h
@@ -3,6 +3,7 @@
 #include <array>
 #include <vector>
 #include <bitset>
+#include <stdexcept>
 
 using namespace std;
 
@@ -20,3 +21,6 @@ uint64_t decrypt(uint64_t state, uint64_t addRoundKeys[32]);
 
 vector<uint64_t> generateRoundKeys80(bitset<80> key);
 vector<uint64_t> generateRoundKeys128(bitset<128> key);
+
+vector<uint8_t> encryptData(const vector<uint8_t>& plain, uint64_t roundKeys[32]);
+vector<uint8_t> decryptData(const vector<uint8_t>& cipher, uint64_t roundKeys[32]);
