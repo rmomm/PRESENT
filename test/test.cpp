5078924687df3bba505034499d170675e63badc0
Karina Malij <karinamalij0303@gmail.com>
2026-04-20 09:52:25 +0300

    tests EncryptZero 80 and 128 bit

--- test/test.cpp
+++ test/test.cpp
@@ -2,26 +2,43 @@
 #include "../PRESENT/present.h"
 #include "../PRESENT/files.h"
 
+using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
-
-TEST(PRESENT80, EncryptZero) {
+TEST(PRESENT80, EncryptZero80) {
     uint64_t plaintext = 0x0000000000000000ULL;
-    std::bitset<80> key;
+    bitset<80> key;
     key.reset();
 
     auto keys = generateRoundKeys80(key);
-    uint64_t cipher = encrypt(plaintext, keys.data());
+    uint64_t rk[32];
+    for (int i = 0; i < 32; i++) { 
+        rk[i] = keys[i]; 
+    }
+
+    uint64_t cipher = encrypt(plaintext, rk);
 
     EXPECT_EQ(cipher, 0x5579C1387B228445ULL);
 }
 
+TEST(PRESENT128, EncryptZero128) {
+    uint64_t plaintext = 0x0000000000000000ULL;
+    bitset<128> key;
+    key.reset();
+
+    auto keys = generateRoundKeys128(key);
+    uint64_t rk[32];
+    for (int i = 0; i < 32; i++) { 
+        rk[i] = keys[i];
+    }
+
+    uint64_t cipher = encrypt(plaintext, rk);
+
+    EXPECT_EQ(cipher, 0x96DB702A2E6900AFULL);
+}
+
 TEST(PRESENT80, DecryptBack) {
     uint64_t plaintext = 0x0000000000000000ULL;
-    std::bitset<80> key;
+    bitset<80> key;
     key.reset();
 
     auto keys = generateRoundKeys80(key);
@@ -30,3 +47,4 @@ TEST(PRESENT80, DecryptBack) {
 
     EXPECT_EQ(decrypted, plaintext);
 }
+
